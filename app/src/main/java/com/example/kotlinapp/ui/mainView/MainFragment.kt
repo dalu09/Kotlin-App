509f2a50ea9911d1bc3224e376bad7296794681f
Juan Camilo Ibáñez <juancibanez2003@gmail.com>
2025-10-03 18:12:41 -0500

    Cambios para profile

--- app/src/main/java/com/example/kotlinapp/ui/mainView/MainFragment.kt
+++ app/src/main/java/com/example/kotlinapp/ui/mainView/MainFragment.kt
@@ -1,4 +1,4 @@
-package com.example.kotlinapp.ui
+package com.example.kotlinapp.ui.mainView
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.kotlinapp.R
 
-class ProfileFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
