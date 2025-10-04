933b70a2b66b00ad97a62b72d8935889ca28548b
Juan Camilo Ibáñez <juancibanez2003@gmail.com>
2025-10-04 12:47:21 -0500

    cambios pequeños

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,12 +54,8 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
-
     implementation("com.google.firebase:firebase-analytics")
-
-
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
-
     implementation("com.google.firebase:firebase-storage")
 }
