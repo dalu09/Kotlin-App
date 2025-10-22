de35787d1b7eecd0f1ac0866e38d9302eb5e43b9
Juanvalencia112 <j.valenciam@uniandes.edu.co>
2025-10-21 20:43:18 -0500

    Cantidad deportes

--- app/src/main/java/com/example/kotlinapp/data/service/AuthServiceAdapter.kt
+++ app/src/main/java/com/example/kotlinapp/data/service/AuthServiceAdapter.kt
@@ -3,24 +3,43 @@ package com.example.kotlinapp.data.serviceadapter
 import com.example.kotlinapp.data.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
 class AuthServiceAdapter {
 
-
     private val firebaseAuth = FirebaseAuth.getInstance()
     private val firestore = FirebaseFirestore.getInstance()
-
-
 
     suspend fun createUserInAuth(email: String, password: String): FirebaseUser {
         val authResult = firebaseAuth.createUserWithEmailAndPassword(email, password).await()
         return authResult.user ?: throw Exception("Error al crear el usuario en Authentication.")
     }
 
+
     suspend fun createUserProfileInFirestore(user: User) {
-        firestore.collection("users").document(user.uid).set(user).await()
+
+        val batch = firestore.batch()
+
+
+        val userRef = firestore.collection("users").document(user.uid)
+        batch.set(userRef, user)
+
+
+        for (sport in user.sportList) {
+
+            val sportCountRef = firestore.collection("sport_counts").document(sport)
+
+
+            val increment = FieldValue.increment(1)
+
+
+            batch.update(sportCountRef, "userCount", increment)
+        }
+
+
+        batch.commit().await()
     }
 
     suspend fun signIn(email: String, password: String): FirebaseUser {
